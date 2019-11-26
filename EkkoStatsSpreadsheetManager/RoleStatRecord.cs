5e685cfe1f1e9c66b2173f6f9a0e8c449980b520
AlFas <AlFasGD@users.noreply.github.com>
2019-11-26 02:51:09 +0200

    Add matches stat in RoleStatRecord

--- EkkoStatsSpreadsheetManager/RoleStatRecord.cs
+++ EkkoStatsSpreadsheetManager/RoleStatRecord.cs
@@ -4,12 +4,13 @@
     {
         public float WinRate;
         public float PickRate;
+        public int Matches;
 
         public float WeightedWinRate => WinRate * PickRate;
 
         public string WinRateString => $"{WinRate:N2}%";
         public string PickRateString => $"{PickRate:N1}%";
 
-        public RoleStatRecord(float winRate, float pickRate) => (WinRate, PickRate) = (winRate, pickRate);
+        public RoleStatRecord(float winRate, float pickRate, int matches) => (WinRate, PickRate, Matches) = (winRate, pickRate, matches);
     }
 }
